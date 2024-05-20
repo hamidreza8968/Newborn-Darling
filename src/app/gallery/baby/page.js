18a6f2c298b79482872e2eced83673e89835b029
mansoure <sorihedayat72@gmail.com>
2024-05-20 15:48:19 +0200

    WIP/ home page , gallery page , parents gallery and baby gallery pages

---
+++ src/app/gallery/baby/page.js
@@ -0,0 +1,9 @@
+export default function BabyGallery() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h1 className="font-bold text-6xl">Baby's Gallery</h1>
+            </div>
+        </main>
+    );
+}
---
+++ src/app/gallery/parents/page.js
@@ -0,0 +1,9 @@
+export default function ParentsGallery() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h1 className="font-bold text-6xl">Parents Gallery</h1>
+            </div>
+        </main>
+    );
+}
