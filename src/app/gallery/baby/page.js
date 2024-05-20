6dfaea41a3acbc3adf3cae79132290460683ebcd
mansoure <sorihedayat72@gmail.com>
2024-05-20 15:51:45 +0200

    WIP/ home page , gallery page , parents gallery and baby gallery pages

--- src/app/gallery/baby/page.js
+++ src/app/gallery/baby/page.js
@@ -2,7 +2,7 @@ export default function BabyGallery() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <h1 className="font-bold text-6xl">Baby's Gallery</h1>
+                <h1 className="font-bold text-6xl">Baby Gallery</h1>
             </div>
         </main>
     );
