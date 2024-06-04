fe5750fa9aa0d154569b8fdd01bca40856dd5d7a
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2024-06-04 13:39:23 +0200

    add/ add bg image to main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import {useRouter} from "next/navigation";
 import Counter from "../components/counter/Counter";
 import Button from "../components/button/Button";
+import backImg from "../../public/images/mainPage/backGrImg.jpg"
+
 
 export default function Home() {
     const rout = useRouter();
@@ -9,7 +11,8 @@ export default function Home() {
         rout.push("/gallery");
     }
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main style= {{ backgroundImage: `url(${backImg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+              className={`flex min-h-screen flex-col items-center justify-between p-24`}>
             <div>
                 <h1 className="font-bold text-6xl">Newborn Darling Coming Soon</h1>
             </div>
